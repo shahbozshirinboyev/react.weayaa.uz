d8eb7439d339b5f8b3d8da97c11089de1ed1de41
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-04 16:26:46 +0500

    Adding Foother Info data and Finished Foother

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,42 @@
 import { FooterInfo } from "../data/data";
 
 function Footer() {
-  console.log(FooterInfo);
   return (
     <section className="bg-mainColor">
       <div className="container p-4 text-white text-[8px] lg:text-[14px]">
-        
-          <img src={FooterInfo[0].logoWhite} alt="logo" className="pb-4 w-[150px]" />
+        <img
+          src={FooterInfo[0].logoWhite}
+          alt="logo"
+          className="pb-4 w-[150px]"
+        />
 
-          <div className="pb-1">
-            <span className="opacity-80">Republic of KOREA: </span>
-            <span>{FooterInfo[0].koreanCompanyName}</span>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="opacity-80">CEO: </span>
-            <span>{FooterInfo[0].koreanCompanyCEO}</span>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="opacity-80">Registration Number: </span>
-            <span>{FooterInfo[0].koreanCompanyRegistrationNumber}</span>
-          </div>
-
-          <div className="pb-1">
-            <span className="opacity-80">Uzbekistan: </span>
-            <span>{FooterInfo[0].uzbCompanyName}</span>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="opacity-80">CEO: </span>
-            <span>{FooterInfo[0].uzbCompanyCEO}</span>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span className="opacity-80">Registration Number: </span>
-            <span>{FooterInfo[0].uzbCompanyRegistrationNumber}</span>
-          </div>
-
-          <div>
-            <span className="opacity-80">Copyright&#174; </span>
-            <span>{FooterInfo[0].copyright}</span>
-          </div>
+        <div className="pb-1">
+          <span className="opacity-80">Republic of KOREA: </span>
+          <span>{FooterInfo[0].koreanCompanyName}</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="opacity-80">CEO: </span>
+          <span>{FooterInfo[0].koreanCompanyCEO}</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="opacity-80">Registration Number: </span>
+          <span>{FooterInfo[0].koreanCompanyRegistrationNumber}</span>
         </div>
-      
+
+        <div className="pb-1">
+          <span className="opacity-80">Uzbekistan: </span>
+          <span>{FooterInfo[0].uzbCompanyName}</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="opacity-80">CEO: </span>
+          <span>{FooterInfo[0].uzbCompanyCEO}</span>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="opacity-80">Registration Number: </span>
+          <span>{FooterInfo[0].uzbCompanyRegistrationNumber}</span>
+        </div>
+
+        <div>
+          <span className="opacity-80">Copyright&#174; </span>
+          <span>{FooterInfo[0].copyright}</span>
+        </div>
+      </div>
     </section>
   );
 }
