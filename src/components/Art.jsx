39ab148e43c632281608d09a1d567c914b34debe
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-05 15:27:07 +0500

    Adding yet another lightbox

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -14,17 +14,25 @@ import "swiper/css/free-mode";
 
 import "./art.css";
 
+// npm install yet-another-react-lightbox
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
+
 // import required modules
 import { FreeMode } from "swiper/modules";
 
 const Art = forwardRef((props, ref) => {
-  const [activeTab, setActiveTab] = useState(0)
+  const [activeTab, setActiveTab] = useState(0);
   const [images, setImages] = useState(galleryInfo[0].counts);
   const filterTab = (tab) => {
     setImages(tab);
     // console.log(tab);
     console.log(images);
   };
+  const [open, setOpen] = useState(false);
+
+  const [activeImg, setActiveImg] = useState("");
   return (
     <section ref={ref} className="container p-4 pt-0 pb-[25px] select-none">
       <div>
@@ -45,9 +53,14 @@ const Art = forwardRef((props, ref) => {
             <SwiperSlide key={gallery.id}>
               <button
                 onClick={() => {
-                  filterTab(gallery.counts); setActiveTab(gallery.id)
+                  filterTab(gallery.counts);
+                  setActiveTab(gallery.id);
                 }}
-                className={`border-transparent font-semibold ${ activeTab === gallery.id ? "text-white bg-mainColor" : "text-mainColor bg-mainColor bg-opacity-30"} transition-all duration-300  px-2 py-1 rounded-lg text-center text`}
+                className={`border-transparent font-semibold ${
+                  activeTab === gallery.id
+                    ? "text-white bg-mainColor"
+                    : "text-mainColor bg-mainColor bg-opacity-30"
+                } transition-all duration-300  px-2 py-1 rounded-lg text-center text`}
               >
                 {gallery.name}
               </button>
@@ -56,13 +69,45 @@ const Art = forwardRef((props, ref) => {
         </Swiper>
       </>
       <>
-      <div className="columns-2 lg:columns-3 xl:columns-4 py-8">
+        <div className="columns-2 lg:columns-3 xl:columns-4 py-8">
           {images.map((item) => (
-            <div key={item.id} className="m-2 transition-all duration-300 cursor-pointer">
-              <img className="w-full transition-all duration-300 rounded-lg shadow-md hover:opacity-50" src={item.img} alt={item.title} />
+            <div
+              key={item.id}
+              className="m-2 transition-all duration-300 cursor-pointer"
+            >
+              <img
+                onClick={() => {
+                  setOpen(true);
+                  setActiveImg(item.img);
+                }}
+                className="w-full transition-all duration-300 rounded-lg shadow-md hover:opacity-50"
+                src={item.img}
+                alt={item.title}
+              />
             </div>
           ))}
-      </div>
+        </div>
+      </>
+      <>
+        <Lightbox
+          open={open}
+          plugins={[Zoom]}
+          close={() => setOpen(false)}
+          slides={[{ src: `${activeImg}` }]}
+          carousel={{ finite: true }}
+          styles={{ container: { backgroundColor: "rgba(0, 0, 0, .9)" } }}
+          render={{
+            buttonPrev: () => null, // Chapga o'tkazuvchi tugmani o'chiradi
+            buttonNext: () => null, // O'ngga o'tkazuvchi tugmani o'chiradi
+          }}
+          zoom={{
+            maxZoomPixelRatio: 5, // Zoom imkoniyatlarini oshiradi (bu qiymatni oshirishingiz mumkin)
+            zoomInMultiplier: 2, // Zoom bosqichlari tezligini boshqaradi
+            doubleTapDelay: 300, // Ikki marta bosish uchun kechikish vaqti (ms)
+            doubleClickDelay: 300, // Ikki marta bosish uchun kechikish vaqti (ms)
+            scrollToZoom: true, // Skrin qilish orqali zoom qilish imkoniyati
+          }}
+        />
       </>
     </section>
   );
