0168eaba1999b6863ee8490a4f9ecffd76b6e961
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-05 15:16:37 +0500

    Adding active menu bg color

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -18,6 +18,7 @@ import "./art.css";
 import { FreeMode } from "swiper/modules";
 
 const Art = forwardRef((props, ref) => {
+  const [activeTab, setActiveTab] = useState(0)
   const [images, setImages] = useState(galleryInfo[0].counts);
   const filterTab = (tab) => {
     setImages(tab);
@@ -44,9 +45,9 @@ const Art = forwardRef((props, ref) => {
             <SwiperSlide key={gallery.id}>
               <button
                 onClick={() => {
-                  filterTab(gallery.counts);
+                  filterTab(gallery.counts); setActiveTab(gallery.id)
                 }}
-                className="border border-mainColor font-semibold text-white bg-mainColor px-2 py-1 rounded-lg text-center text"
+                className={`border-transparent font-semibold ${ activeTab === gallery.id ? "text-white bg-mainColor" : "text-mainColor bg-mainColor bg-opacity-30"} transition-all duration-300  px-2 py-1 rounded-lg text-center text`}
               >
                 {gallery.name}
               </button>
@@ -57,7 +58,7 @@ const Art = forwardRef((props, ref) => {
       <>
       <div className="columns-2 lg:columns-3 xl:columns-4 py-8">
           {images.map((item) => (
-            <div key={item.id} className="m-2 transition-all duration-300">
+            <div key={item.id} className="m-2 transition-all duration-300 cursor-pointer">
               <img className="w-full transition-all duration-300 rounded-lg shadow-md hover:opacity-50" src={item.img} alt={item.title} />
             </div>
           ))}
