08e4f88139dcddee279500d01cfec3cdb94f18d6
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-01-08 14:48:16 +0500

    new tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: { center: true },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         mainColor: "#2E6734",
       },
       columns: {
-        1: "1", // column-count: 1 uchun
-        2: "2", // column-count: 1 uchun
-        3: "3", // column-count: 1 uchun
-        4: "4", // column-count: 1 uchun
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
       },
     },
   },
-  plugins: [require('daisyui'), ],  daisyui: { darkTheme: false, },
+  plugins: [require("daisyui")],
+  daisyui: { darkTheme: false },
 };
