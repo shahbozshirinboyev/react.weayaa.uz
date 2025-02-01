d0e0d47179219fe31438765f8f3241b024f8e715
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-01 13:28:31 +0500

    Navbar section ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,12 +45,12 @@ function Navbar({ setNavRef }) {
   const navRef = useRef(null);
 
   useEffect(() => {
-    setNavRef(navRef?.current?.offsetHeight)
-  }, [navRef?.current?.offsetHeight])
+    setNavRef(navRef?.current?.offsetHeight);
+  }, [navRef?.current?.offsetHeight]);
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = menus.map(menu => document.querySelector(menu.href));
+      const sections = menus.map((menu) => document.querySelector(menu.href));
       const scrollPosition = window.scrollY + 100;
 
       sections.forEach((section, index) => {
@@ -72,24 +72,41 @@ function Navbar({ setNavRef }) {
   }, [scrollY]);
 
   return (
-    <section ref={navRef}
+    <section
+      ref={navRef}
       className={`fixed w-full px-2 z-[999] transition-all duration-300
-        ${scrollY > 50 ? "py-2 bg-white bg-opacity-90 shadow-md" : "py-4 bg-transparent shadow-none"}`}
+        ${
+          scrollY > 50
+            ? "py-2 bg-white bg-opacity-[95%] shadow-md"
+            : "py-4 bg-transparent shadow-none"
+        }`}
     >
-      <ul className="container flex justify-between items-center mx-auto gap-4 text-center border border-red-700">
-
-        <li className="btn bg-transparent shadow-0 border hover:bg-transparent border-red-700">
-          <img src={scrollY > 50 ? logoBlack : logoWhite} alt="weayaa_logo" className="transition-all duration-300 w-[130px]" />
+      <ul className="container flex justify-between items-center mx-auto gap-4 text-center">
+        <li>
+          <a
+            href="#home"
+            className="btn bg-transparent shadow-0 border-0 hover:bg-transparent shadow-none"
+          >
+            <img
+              src={scrollY > 50 ? logoBlack : logoWhite}
+              alt="weayaa_logo"
+              className="transition-all duration-300 w-[130px]"
+            />
+          </a>
         </li>
 
         <div className="flex gap-4">
           {menus.map((menu) => (
-            <li key={menu.id} className={`hidden md:flex border border-red-700`} >
+            <li key={menu.id} className={`hidden md:flex`}>
               <a
                 href={menu.href}
                 onClick={() => setActiveId(menu.id)}
                 className={`cursor-pointer font-semibold transition-all duration-300 w-[80px] hover:text-white hover:bg-mainColor btn btn-sm border-0
-                ${scrollY > 50 ? "text-mainColor bg-mainColor bg-opacity-20" : "text-white bg-opacity-0"}
+                ${
+                  scrollY > 50
+                    ? "text-mainColor bg-mainColor bg-opacity-20"
+                    : "text-white bg-opacity-0"
+                }
                 ${activeId === menu.id ? "!bg-mainColor !text-white" : ""}`}
               >
                 {menu.name}
@@ -98,14 +115,15 @@ function Navbar({ setNavRef }) {
           ))}
         </div>
 
-        <li className="cursor-pointer text-end hidden lg:block border border-red-700">
+        <li className="cursor-pointer text-end hidden lg:block">
           <div className="relative inline-block text-left font-bold">
             <div className="relative">
               <button
                 onClick={() => setLanguageBrn(!languageBtn)}
                 type="button"
-                className={`${scrollY > 50 ? "text-mainColor" : "text-white"
-                  } inline-flex w-full justify-center gap-x-1.5 px-3 py-2 transition-all duration-300 btn bg-transparent border-0 btn-sm hover:bg-mainColor hover:text-white`}
+                className={`${
+                  scrollY > 50 ? "text-mainColor" : "text-white"
+                } inline-flex w-full justify-center gap-x-1.5 px-3 py-2 transition-all duration-300 btn bg-transparent border-0 btn-sm hover:bg-mainColor hover:text-white`}
               >
                 En
                 {!languageBtn && <i className="bi bi-chevron-down"></i>}
@@ -149,17 +167,15 @@ function Navbar({ setNavRef }) {
                   >
                     Korean
                   </a>
-                  <form method="POST" action="#" role="none">
-                    <button
-                      type="submit"
-                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-green-100"
-                      role="menuitem"
-                      tabIndex="-1"
-                      id="menu-item-3"
-                    >
-                      Russian
-                    </button>
-                  </form>
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-green-100"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-2"
+                  >
+                    Russian
+                  </a>
                 </div>
               </div>
             )}
@@ -168,8 +184,9 @@ function Navbar({ setNavRef }) {
 
         <li
           onClick={() => setOpen(!open)}
-          className={`text-end w-[50px] text-[30px] flex justify-center items-center ${scrollY > 50 ? "text-mainColor" : "text-white"
-            } md:hidden cursor-pointer transition-all duration-300`}
+          className={`text-end w-[50px] text-[30px] flex justify-center items-center ${
+            scrollY > 50 ? "text-mainColor" : "text-white"
+          } md:hidden cursor-pointer transition-all duration-300`}
         >
           {!open && (
             <AnimatePresence mode="wait">
@@ -205,15 +222,25 @@ function Navbar({ setNavRef }) {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -300 }}
             transition={{ duration: 0.3, delay: 0.2 }}
-            className={`bg-white shadow-md fixed w-[calc(100%-16px)] p-2 md:hidden rounded-md border ${scrollY > 50 ? "mt-[26px]" : "mt-[26px]"}`}
+            className={`bg-white shadow-md fixed w-[calc(100%-16px)] p-2 md:hidden rounded-md border border-mainColor border-opacity-20 ${
+              scrollY > 50 ? "mt-[26px]" : "mt-[26px]"
+            }`}
           >
             <ul className="items-center text-center p-2">
               {menus.map((menu) => (
                 <li key={menu.id} className="my-2">
                   <a
                     href={menu.href}
-                    onClick={() => { setActiveId(menu.id); setOpen(false); }}
-                    className={`btn btn-sm w-full transition-all flex justify-start items-center duration-300 border-0 ${activeId === menu.id ? "text-white bg-mainColor" : "text-mainColor bg-mainColor bg-opacity-20"}`}>
+                    onClick={() => {
+                      setActiveId(menu.id);
+                      setOpen(false);
+                    }}
+                    className={`btn btn-sm w-full transition-all hover:bg-mainColor hover:bg-opacity-40 flex justify-start items-center duration-300 border-0 ${
+                      activeId === menu.id
+                        ? "text-white bg-mainColor"
+                        : "text-mainColor bg-mainColor bg-opacity-20"
+                    }`}
+                  >
                     <i className={menu.icon}></i>
                     <span>{menu.name}</span>
                   </a>
