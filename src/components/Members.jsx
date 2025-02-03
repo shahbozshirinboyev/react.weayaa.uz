e50f4eba559f784364630caa7f412b2d2b33ec86
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-03 13:50:34 +0500

    Members section ...

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,21 +1,8 @@
-import { forwardRef, useState } from "react";
-import { motion, useAnimation } from "framer-motion";
+import React, { forwardRef } from "react";
+import { motion } from "framer-motion";
 import { MembersInfo } from "../data/data";
 
 const Members = forwardRef((props, ref) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const controls = useAnimation();
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-    controls.stop();
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    controls.start({ x: "-100%" });
-  };
-
   return (
     <section ref={ref} className="h-full">
       <p className="font-semibold text-[24px] text-center my-[25px] text-mainColor">
@@ -25,17 +12,14 @@ const Members = forwardRef((props, ref) => {
       <div className="flex overflow-x-hidden h-full items-center">
         <motion.div
           initial={{ x: 0 }}
-          animate={controls}
+          animate={{ x: "-100%" }}
           transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
           className="flex flex-shrink-0"
-          style={{ width: `${MembersInfo.length * 260}px` }}
         >
           {MembersInfo.map((member, index) => (
             <div
               key={index}
               className={`m-2 border border-mainColor border-opacity-30 w-[260px] rounded-lg grayscale hover:grayscale-0`}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
             >
               <img
                 className="w-full h-[250px] object-cover rounded-t-lg"
@@ -52,17 +36,14 @@ const Members = forwardRef((props, ref) => {
         </motion.div>
         <motion.div
           initial={{ x: 0 }}
-          animate={controls}
+          animate={{ x: "-100%" }}
           transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
           className="flex flex-shrink-0"
-          style={{ width: `${MembersInfo.length * 260}px` }}
         >
           {MembersInfo.map((member, index) => (
             <div
               key={index}
               className={`m-2 border border-mainColor border-opacity-30 w-[260px] rounded-lg grayscale hover:grayscale-0`}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
             >
               <img
                 className="w-full h-[250px] object-cover rounded-t-lg"
