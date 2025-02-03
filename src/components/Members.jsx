98bf73d611f42a6b54bcb693bab4db6423a9bc51
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-03 13:10:35 +0500

    New section ...

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,47 +1,83 @@
-import React, { forwardRef } from "react";
-import { motion } from "framer-motion";
-import { div } from "framer-motion/client";
-// data
+import { forwardRef, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 import { MembersInfo } from "../data/data";
 
 const Members = forwardRef((props, ref) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const controls = useAnimation();
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    controls.stop();
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    controls.start({ x: "-100%" });
+  };
+
   return (
     <section ref={ref} className="h-full">
+      <p className="font-semibold text-[24px] text-center my-[25px] text-mainColor">
+        Members
+      </p>
 
-        <p className="font-semibold text-[24px] text-center my-[25px] text-mainColor">Members</p>
-
-        <div className="flex overflow-x-hidden h-full items-center border border-red-700">
-          <motion.div
-            initial={{ x: 0 }}
-            animate={{ x: "-100%" }}
-            transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
-            className="flex flex-shrink-0"
-            style={{ width: `${MembersInfo.length * 260}px` }}
-          >
-            {MembersInfo.map((member, index) => (
-              <div key={index} className={`m-2 border border-mainColor border-opacity-30 w-[260px] rounded-lg grayscale hover:grayscale-0`} >
-                <img className="w-full h-[250px] object-cover rounded-t-lg" src={member.img} />
-                <h4 className="text-center text-mainColor font-bold mt-2">{member.label}</h4>
-                <h1 className="text-center text-mainColor font-bold text-opacity-60 mb-2">{member.name}</h1>
-              </div>
-            ))}
-          </motion.div>
-          <motion.div
-            initial={{ x: 0 }}
-            animate={{ x: "-100%" }}
-            transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
-            className="flex flex-shrink-0"
-            style={{ width: `${MembersInfo.length * 260}px` }}
-          >
-            {MembersInfo.map((member, index) => (
-              <div key={index} className={`m-2 border border-mainColor border-opacity-30 w-[260px] rounded-lg grayscale hover:grayscale-0`} >
-                <img className="w-full h-[250px] object-cover rounded-t-lg" src={member.img} />
-                <h4 className="text-center text-mainColor font-bold mt-2">{member.label}</h4>
-                <h1 className="text-center text-mainColor font-bold text-opacity-60 mb-2">{member.name}</h1>
-              </div>
-            ))}
-          </motion.div>
-        </div>
+      <div className="flex overflow-x-hidden h-full items-center">
+        <motion.div
+          initial={{ x: 0 }}
+          animate={controls}
+          transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
+          className="flex flex-shrink-0"
+          style={{ width: `${MembersInfo.length * 260}px` }}
+        >
+          {MembersInfo.map((member, index) => (
+            <div
+              key={index}
+              className={`m-2 border border-mainColor border-opacity-30 w-[260px] rounded-lg grayscale hover:grayscale-0`}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                className="w-full h-[250px] object-cover rounded-t-lg"
+                src={member.img}
+              />
+              <h4 className="text-center text-mainColor font-bold mt-2">
+                {member.label}
+              </h4>
+              <h1 className="text-center text-mainColor font-bold text-opacity-60 mb-2">
+                {member.name}
+              </h1>
+            </div>
+          ))}
+        </motion.div>
+        <motion.div
+          initial={{ x: 0 }}
+          animate={controls}
+          transition={{ duration: 50, repeat: Infinity, ease: "linear" }}
+          className="flex flex-shrink-0"
+          style={{ width: `${MembersInfo.length * 260}px` }}
+        >
+          {MembersInfo.map((member, index) => (
+            <div
+              key={index}
+              className={`m-2 border border-mainColor border-opacity-30 w-[260px] rounded-lg grayscale hover:grayscale-0`}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                className="w-full h-[250px] object-cover rounded-t-lg"
+                src={member.img}
+              />
+              <h4 className="text-center text-mainColor font-bold mt-2">
+                {member.label}
+              </h4>
+              <h1 className="text-center text-mainColor font-bold text-opacity-60 mb-2">
+                {member.name}
+              </h1>
+            </div>
+          ))}
+        </motion.div>
+      </div>
     </section>
   );
 });
