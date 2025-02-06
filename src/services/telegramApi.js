14c6f443d0328e376a75982188cead9a413f5e7d
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-06 12:10:46 +0500

    Chat ID change {telegram.org}

--- src/services/telegramApi.js
+++ src/services/telegramApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const token = "7756740360:AAFPLRzyGDHIsj3hrM9GV8VDJwm_HLIJ6Xg";
 const http = axios.create({ baseURL: `https://api.telegram.org/bot${token}`, });
+
 // const chatId = "660100854"; //@sh_shirinboyev
 const chatId = "6729068990";    //@tommykim_uz
 
